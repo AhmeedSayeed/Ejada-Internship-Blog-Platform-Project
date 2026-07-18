42664f5142397a205bd517c7b61749eed8bf0773
Abdelrahman Saber <dadsaber233@gmail.com>
2026-07-18 16:37:05 +0300

    add post and comment DTOs

---
+++ Blog Project/Application/DTOs/Comment.cs
@@ -0,0 +1,30 @@
+﻿namespace Blog_Project.Application.DTOs;
+
+public record CommentDto(
+        int Id,
+        string Content,
+        int PostId,
+        int UserId,
+        string UserName,
+        string? UserProfileImageUrl,
+        DateTime CreatedAt
+    );
+public record CreateCommentDto(
+    string Content,
+    int PostId,
+    int? ParentCommentId
+);
+public record UpdateCommentDto(
+    string Content
+ );
+public record CommentDetailsDto(
+    int Id,
+    string Content,
+    int PostId,
+    int UserId,
+    string UserName,
+    string? UserProfileImageUrl,
+    int? ParentCommentId,
+    DateTime CreatedAt
+);
+
---
+++ Blog Project/Application/DTOs/PostDtos.cs
@@ -0,0 +1,45 @@
+﻿namespace Blog_Project.Application.DTOs;
+
+public record CreatePostDto(
+            string Title,
+            string Content,
+            int? CategoryId
+         );
+public record UpdatePostDto(
+    string Title,
+    string Content,
+    int? CategoryId
+);
+public record PostDto(
+    int Id,
+    string Title,
+    string Author,
+    string? Category,
+    double AvgRating,
+    int ViewCount,
+    DateTime CreatedAt
+);
+public record PostDetailsDto(
+    int Id,
+    string Title,
+    string Content,
+    int? CategoryId,
+    string? CategoryName,
+    int AuthorId,
+    string AuthorUserName,
+    string AuthorEmail,
+    string? AuthorProfileImageUrl,
+    int? ReviewedByAdminId,
+    string? ReviewedByAdminUserName,
+    string? ReviewedByAdminEmail,
+    string? ReviewedByAdminProfileImageUrl,
+    int ViewCount,
+    double AvgRating,
+    int RatingCount,
+    DateTime? SubmittedAt,
+    DateTime? PublishedAt,
+    DateTime CreatedAt,
+    DateTime LastUpdatedAt
+);
+
+
