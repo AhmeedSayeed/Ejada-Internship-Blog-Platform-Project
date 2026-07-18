8b7b557678d4ed7e6f39a643412bee4600417171
Abdelrahman Saber <dadsaber233@gmail.com>
2026-07-18 19:16:54 +0300

    Add Category and Rating DTOs

---
+++ Blog Project/Application/DTOs/Rating.cs
@@ -0,0 +1,21 @@
+﻿namespace Blog_Project.Application.DTOs;
+
+public record CreateRatingDto(
+    int Score,
+    int PostId,
+    int UserId
+
+);
+public record UpdateRatingDto(
+    int Id,
+    int Score
+);
+public record RatingDetailDto(
+    int Id,
+    int Score,
+    int PostId,
+    int UserId,
+    string UserName,
+    string? UserProfileImageUrl,
+    DateTime CreatedAt
+);
