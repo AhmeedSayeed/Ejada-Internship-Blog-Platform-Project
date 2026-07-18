f9d1e36c6ea6abf24510c5b1d526361c2a8cbcbe
Abdelrahman Saber <dadsaber233@gmail.com>
2026-07-18 19:38:37 +0300

    Add Tags Dtos

---
+++ Blog Project/Application/DTOs/PostTag.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog_Project.Application.DTOs;
+
+public record CreatePostTagDto(
+    int PostId,
+    int TagId
+);
+public record UpdatePostTagDto(
+    int PostId,
+    int OldTagId,
+    int NewTagId
+);
