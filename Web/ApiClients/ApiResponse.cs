935a6a55f522ec0af2cd33585d7e33637310f0b5
Ahmed Sayed <as3992054@gmail.com>
2026-07-22 17:56:39 +0300

    Add setup

---
+++ Web/ApiClients/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.ApiClients
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string? ErrorMessage { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+
+    public class ApiResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
---
+++ Web/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+    {
+        [Route("404")]
+        public IActionResult PageNotFound()
+        {
+            return View("404");
+        }
+
+        [Route("500")]
+        public IActionResult ServerError()
+        {
+            return View("500");
+        }
+    }
+}
